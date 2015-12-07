914c3b63e0b453d20b972dccae6c07ce520d543b
sriiskanth <sri.is.kanth@gmail.com>
2015-12-07 23:34:07 +0530

    Binary tree changes

--- DataStructures/src/com/srikanth/Trees/BinarySearchTree.java
+++ DataStructures/src/com/srikanth/Trees/BinarySearchTree.java
@@ -1,5 +1,6 @@
 package com.srikanth.Trees;
 
+
 public class BinarySearchTree {
 	private Node root;
 
@@ -59,4 +60,98 @@ public class BinarySearchTree {
 		
 	}
 	
+	
+	public int findMin(){
+		 return findMin(root);
+	}
+
+	
+	public int findMax(){
+		 return findMax(root);
+	}
+
+	private int  findMin(Node node) {
+		if(node==null)
+			return 0;
+		else if(node.getLeft()!=null)
+			return findMin(node.getLeft());
+		else 
+			return node.getValue();
+		
+	}
+	
+	
+	private int  findMax(Node node) {
+		if(node==null)
+			return 0;
+		else if(node.getRight()!=null)
+			return findMax(node.getRight());
+		else 
+			return node.getValue();
+		
+	}
+	
+	
+	public Node parentNode(int key){
+		return parentNode(root,find(key));
+	}
+
+	private Node parentNode(Node currentNode, Node keyNode) {
+		if(root==keyNode||currentNode==null)
+		return null;
+		else{
+			if(currentNode.getLeft()==keyNode||currentNode.getRight()==keyNode)
+				return currentNode;
+			else {
+				if(currentNode.getValue()<keyNode.getValue())
+					return parentNode(currentNode.getRight(),keyNode);
+				else 
+					return parentNode(currentNode.getLeft(),keyNode);
+			}
+		}
+		
+	}
+	
+	
+	 public void printTree() {
+	        printSubtree(root);
+	    }
+	    
+	    public void printSubtree(Node node) {
+	        if (node.getRight() != null) {
+	            printTree(node.getRight(), true, "");
+	        }
+	        printNodeValue(node);
+	        if (node.getLeft() != null) {
+	            printTree(node.getLeft(), false, "");
+	        }
+	    }
+	    
+	    private void printNodeValue(Node node) {
+	        if (node.getValue() == 0) {
+	            System.out.print("<0>");
+	        } else {
+	            System.out.print(node.getValue());
+	        }
+	        System.out.println();
+	    }
+	    
+	    
+	    private void printTree(Node node, boolean isRight, String indent) {
+	        if (node.getRight() != null) {
+	            printTree(node.getRight(), true, indent + (isRight ? "        " : " *      "));
+	        }
+	        System.out.print(indent);
+	        if (isRight) {
+	            System.out.print(" /");
+	        } else {
+	            System.out.print(" \\");
+	        }
+	        System.out.print("*****");
+	        printNodeValue(node);
+	        if (node.getLeft() != null) {
+	            printTree(node.getLeft(), false, indent + (isRight ? " *      " : "        "));
+	        }
+	    }
+
 }
--- DataStructures/src/com/srikanth/Trees/TestBinaryTree.java
+++ DataStructures/src/com/srikanth/Trees/TestBinaryTree.java
@@ -7,10 +7,20 @@ public class TestBinaryTree {
 	public static void main(String[] args) {
 		BinarySearchTree btree=new BinarySearchTree();
 		int[] arr=Util.getArray(10);
+		
+		//int[] arr={6,1,8,23,4,87,12,7,21,45,78,100,13};
 		for (int i = 0; i < arr.length; i++) {
 			btree.add(arr[i]);
 		}
 		btree.inOrder();
+		System.out.println(); 
+		System.out.println(); 
+		System.out.println(); 
+		btree.printTree();
+		System.out.println("Min	:	"+btree.findMin());
+		System.out.println("Max	:	"+btree.findMax());
+		//Node parent=btree.parentNode(100);
+		//System.out.println("Parent	:	"+parent.getValue());
 	}
 
 }
