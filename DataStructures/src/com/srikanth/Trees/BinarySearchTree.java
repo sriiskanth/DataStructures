63e4a78011a48430518054f8f9594d921f843b2c
sriiskanth <sri.is.kanth@gmail.com>
2015-12-12 01:06:54 +0530

    Added Delete functionality

--- DataStructures/src/com/srikanth/Trees/BinarySearchTree.java
+++ DataStructures/src/com/srikanth/Trees/BinarySearchTree.java
@@ -1,6 +1,5 @@
 package com.srikanth.Trees;
 
-
 public class BinarySearchTree {
 	private Node root;
 
@@ -11,147 +10,233 @@ public class BinarySearchTree {
 	public void setRoot(Node root) {
 		this.root = root;
 	}
-	
-	
-	public Node  find(int key){
-		return find(this.root,key);
+
+	public Node find(int key) {
+		return find(this.root, key);
 	}
 
 	private Node find(Node node, int key) {
-		if(node==null)
+		if (node == null)
 			return null;
-		else if (key<node.getValue())
-			find(node.getLeft(),key);
-		else if(key>node.getValue())
-			find(node.getRight(),key);
-		else return node;
+		else {
+			if (key < node.getValue())
+				return find(node.getLeft(), key);
+			else if (key > node.getValue())
+				return find(node.getRight(), key);
+			else
+				return node;
+		}
 
-		return null;
 	}
-	
-	
-	public void add(int key){
-		this.root=add(this.root,key);
+
+	public void add(int key) {
+		this.root = add(this.root, key);
 	}
 
 	private Node add(Node node, int key) {
-		if(node==null)
+		if (node == null)
 			return new Node(key);
-		else if(key<node.getValue())
-			node.setLeft(add(node.getLeft(),key));
-		else if(key>node.getValue())
-			node.setRight(add(node.getRight(),key));
+		else if (key < node.getValue())
+			node.setLeft(add(node.getLeft(), key));
+		else if (key > node.getValue())
+			node.setRight(add(node.getRight(), key));
 		else
 			node.setValue(key);
 		return node;
 	}
-	
-	public void inOrder(){
+
+	public void inOrder() {
 		inOrder(this.root);
 	}
-	
+
 	private void inOrder(Node node) {
-		if(node!=null){
+		if (node != null) {
 			inOrder(node.getLeft());
 			System.out.println(node.getValue());
 			inOrder(node.getRight());
-				
+
 		}
-		
-	}
-	
-	
-	public int findMin(){
-		 return findMin(root);
+
 	}
 
-	
-	public int findMax(){
-		 return findMax(root);
+	public int findMin() {
+		return findMin(root);
 	}
 
-	private int  findMin(Node node) {
-		if(node==null)
+	public int findMax() {
+		return findMax(root);
+	}
+
+	private int findMin(Node node) {
+		if (node == null)
 			return 0;
-		else if(node.getLeft()!=null)
+		else if (node.getLeft() != null)
 			return findMin(node.getLeft());
-		else 
+		else
 			return node.getValue();
-		
+
 	}
-	
-	
-	private int  findMax(Node node) {
-		if(node==null)
+
+	private int findMax(Node node) {
+		if (node == null)
 			return 0;
-		else if(node.getRight()!=null)
+		else if (node.getRight() != null)
 			return findMax(node.getRight());
-		else 
+		else
 			return node.getValue();
-		
+
 	}
-	
-	
-	public Node parentNode(int key){
-		return parentNode(root,find(key));
+
+	public Node parentNode(int key) {
+		return parentNode(root, find(key));
 	}
 
 	private Node parentNode(Node currentNode, Node keyNode) {
-		if(root==keyNode||currentNode==null)
-		return null;
-		else{
-			if(currentNode.getLeft()==keyNode||currentNode.getRight()==keyNode)
+		if (currentNode == keyNode || currentNode == null)
+			return null;
+		else {
+			if (currentNode.getLeft() == keyNode || currentNode.getRight() == keyNode)
 				return currentNode;
 			else {
-				if(currentNode.getValue()<keyNode.getValue())
-					return parentNode(currentNode.getRight(),keyNode);
-				else 
-					return parentNode(currentNode.getLeft(),keyNode);
+				if (currentNode.getValue() < keyNode.getValue())
+					return parentNode(currentNode.getRight(), keyNode);
+				else
+					return parentNode(currentNode.getLeft(), keyNode);
 			}
+		}
+
+	}
+
+	// Finding successor
+	/*
+	 * 
+	 * 1.if x:right = NIL 2.return TREE-MINIMUM.x:right/ 3.y D x:p 4 while y ¤
+	 * NIL and x == y:right 5 x D y 6 y D y:p 7 return y
+	 * 
+	 */
+
+	public int successor(int key) {
+		return successor(find(key));
+	}
+
+	private int successor(Node node) {
+		if (node.getRight() != null)
+			return findMin(node.getRight());
+		else {
+			Node y = parentNode(node.getValue());
+			Node x = node;
+			while (parentNode(x.getValue()) != null & parentNode(x.getValue()).getRight() == x) {
+				x = y;
+				y = parentNode(y.getValue());
+			}
+			return y.getValue();
+		}
+
+	}
+
+	public void printTree() {
+		printSubtree(root);
+	}
+
+	public void printSubtree(Node node) {
+		if (node.getRight() != null) {
+			printTree(node.getRight(), true, "");
+		}
+		printNodeValue(node);
+		if (node.getLeft() != null) {
+			printTree(node.getLeft(), false, "");
+		}
+	}
+
+	private void printNodeValue(Node node) {
+		if (node.getValue() == 0) {
+			System.out.print("<0>");
+		} else {
+			System.out.print(node.getValue());
+		}
+		System.out.println();
+	}
+
+	private void printTree(Node node, boolean isRight, String indent) {
+		if (node.getRight() != null) {
+			printTree(node.getRight(), true, indent + (isRight ? "        " : " *      "));
+		}
+		System.out.print(indent);
+		if (isRight) {
+			System.out.print(" /");
+		} else {
+			System.out.print(" \\");
+		}
+		System.out.print("*****");
+		printNodeValue(node);
+		if (node.getLeft() != null) {
+			printTree(node.getLeft(), false, indent + (isRight ? " *      " : "        "));
+		}
+	}
+
+	public void delete(int key) {
+		// delete(root,key);
+		delete(find(root,key));
+	}
+
+	private Node delete(Node deleteNode) {
+		int key = deleteNode.getValue();
+		Node parentNode=parentNode(deleteNode.getValue());
+		if (deleteNode == null) {
+			System.out.println("Value Not present");
+			return null;
+		}
+		
+		if(deleteNode.getLeft()==null&&deleteNode.getRight()==null){
+			return deleteLeaf(deleteNode);
+		}
+		else if(deleteNode.getLeft()==null||deleteNode.getRight()==null){
+			Node singleChildNode=singleChildNode(deleteNode);
+			boolean isLeftParent=isLeftParent(parentNode,deleteNode);
+			if(!isLeftParent)
+				parentNode.setLeft(singleChildNode);
+			else
+				parentNode.setRight(singleChildNode);
+			return deleteNode;
+			
+		}
+		else /*if(deleteNode.getLeft()!=null&&deleteNode.getRight()!=null)*/{
+			Node successor=find(successor(deleteNode.getValue()));
+			deleteNode.setValue(successor.getValue());
+			deleteNode.setRight(delete(successor));
+			return deleteNode;
 		}
 		
 	}
-	
-	
-	 public void printTree() {
-	        printSubtree(root);
-	    }
-	    
-	    public void printSubtree(Node node) {
-	        if (node.getRight() != null) {
-	            printTree(node.getRight(), true, "");
-	        }
-	        printNodeValue(node);
-	        if (node.getLeft() != null) {
-	            printTree(node.getLeft(), false, "");
-	        }
-	    }
-	    
-	    private void printNodeValue(Node node) {
-	        if (node.getValue() == 0) {
-	            System.out.print("<0>");
-	        } else {
-	            System.out.print(node.getValue());
-	        }
-	        System.out.println();
-	    }
-	    
-	    
-	    private void printTree(Node node, boolean isRight, String indent) {
-	        if (node.getRight() != null) {
-	            printTree(node.getRight(), true, indent + (isRight ? "        " : " *      "));
-	        }
-	        System.out.print(indent);
-	        if (isRight) {
-	            System.out.print(" /");
-	        } else {
-	            System.out.print(" \\");
-	        }
-	        System.out.print("*****");
-	        printNodeValue(node);
-	        if (node.getLeft() != null) {
-	            printTree(node.getLeft(), false, indent + (isRight ? " *      " : "        "));
-	        }
-	    }
+
+	private boolean isLeftParent(Node parentNode,Node childNode) {
+		if(childNode.getValue()<parentNode.getValue())
+			return false;
+		else
+			return true;
+	}
+
+	private Node singleChildNode(Node node) {
+		if(node.getLeft()!=null)
+			return node.getLeft();
+		else
+			return node.getRight();
+	}
+
+	private Node deleteLeaf(Node leafNode) {
+		Node parentNode=parentNode(leafNode.getValue());
+		System.out.println("Parent :"+parentNode.getValue());
+		System.out.println("Leaf   :"+leafNode.getValue());
+		if(leafNode.getValue()<parentNode.getValue())
+			parentNode.setLeft(null);
+		else
+			parentNode.setRight(null);
+		System.out.println("Deleted Leaf Node	:	"+leafNode.getValue());
+		return leafNode;
+		
+		
+	}
+
+
 
 }
--- DataStructures/src/com/srikanth/Trees/TestBinaryTree.java
+++ DataStructures/src/com/srikanth/Trees/TestBinaryTree.java
@@ -6,9 +6,9 @@ public class TestBinaryTree {
 
 	public static void main(String[] args) {
 		BinarySearchTree btree=new BinarySearchTree();
-		int[] arr=Util.getArray(10);
+		//int[] arr=Util.getArray(10);
 		
-		//int[] arr={6,1,8,23,4,87,12,7,21,45,78,100,13};
+		int[] arr={6,1,8,23,4,87,12,7,21,45,78,100,13};
 		for (int i = 0; i < arr.length; i++) {
 			btree.add(arr[i]);
 		}
@@ -17,10 +17,41 @@ public class TestBinaryTree {
 		System.out.println(); 
 		System.out.println(); 
 		btree.printTree();
-		System.out.println("Min	:	"+btree.findMin());
-		System.out.println("Max	:	"+btree.findMax());
-		//Node parent=btree.parentNode(100);
+		//System.out.println("Min	:	"+btree.findMin());
+		//System.out.println("Max	:	"+btree.findMax());
+		//System.out.println("Find	:"+btree.find(21).getValue());
+		//Node parent=btree.parentNode(78);
 		//System.out.println("Parent	:	"+parent.getValue());
+		//System.out.println("Successor	:	"+btree.successor(13));
+
+
+		/*
+		 
+                         /*****100
+                 /*****87
+                 *       *       /*****78
+                 *       \*****45
+         /*****23
+         *       *       /*****21
+         *       *       *       \*****13
+         *       \*****12
+ /*****8
+ *       \*****7
+6
+ *       /*****4
+ \*****1
+
+
+		 */
+		
+		btree.delete(45);
+		System.out.println();System.out.println();
+		btree.printTree();
+		//btree.delete(78);
+		//btree.printTree();
+		
+//		
+		
 	}
 
 }
